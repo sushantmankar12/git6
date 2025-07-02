0776d8a174c34f8eccf642975b96366434007d13
sushant <sushantmankar2@gmail.com>
2025-07-02 11:03:10 +0530

    Revert "app.py"
    This reverts commit 91ba50d47df3bcfa5f0b5065f6407636084e4def.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ public class UserInputExample {
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
 
-        System.out.println("Hello, " + nayan + "!");
+        System.out.println("Hello, " + sahil + "!");
 
         scanner.close();
     }
